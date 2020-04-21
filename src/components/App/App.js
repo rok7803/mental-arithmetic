ec25cd51ebc0587e03902886ccb2b3083f1e008b
ha6icht <rolf.karlen@gmail.com>
2020-04-21 18:25:23 +0200

    Update App.js cookies

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
+import Cookies from 'universal-cookie';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showCookiePolicy: true,
+    }
+    //this.setCookie = this.setCookie.bind(this);
+    this.setMyCat = this.setMyCat.bind(this);
+    this.setPolicy = this.setPolicy.bind(this);
+    this.removeMyCat = this.removeMyCat.bind(this);
+    this.setCookiePolicyUnvisible = this.setCookiePolicyUnvisible.bind(this);
+  }
+  componentDidMount(){
+    this.getCookie('cookiePolicy');
+  }
+  setCookie(cookieName){
+    const cookies = new Cookies();
+    const isSet = true;
+    let d = new Date();
+    d.setTime(d.getTime() + (90*24*60*60*1000));
+
+
+    cookies.set(cookieName, isSet, { path: '/', expires: d});
+    console.log(cookies.get(cookieName));
+    if(cookieName === 'cookiePolicy') setInterval(this.setCookiePolicyUnvisible, 2000);
+  }
+  setMyCat(){
+    this.setCookie('myCat');
+  }
+  setPolicy(){
+    this.setCookie('cookiePolicy');
+  }
+  getCookie(cookieName){
+    const cookies = new Cookies();
+
+    const cookieValue = cookies.get(cookieName);
+    if(cookieValue) this.setState({
+      showCookiePolicy: false,
+    });
+    //console.log('In getCookie');
+  }
+  removeCookie(cookieName){
+    const cookies = new Cookies();
+
+    cookies.remove(cookieName,{ path: '/'});
+    console.log('Cookie Removed', cookies.get(cookieName));
+  }
+  removeMyCat(){
+    this.removeCookie('myCat');
+  }
+  setCookiePolicyUnvisible(){
+    this.setState({
+      showCookiePolicy: false,
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <button onClick={this.setMyCat}>Set Cookie</button>
+        <button onClick={this.removeMyCat}>Remove Cookie</button>
+        {this.state.showCookiePolicy && <div className="Cookie-Policy">
+          <p>This site uses cookies. To use this site proberly, you have to accept cookies.</p>
+          <button onClick={this.setPolicy}>Accept Cookies</button>
+        </div>}
+      </div>
+    );
+  }
 }
 
 export default App;
