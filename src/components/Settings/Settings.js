6f0514bc54b743af7c52e15d83e8ce1c5d175f17
ha6icht <rolf.karlen@gmail.com>
2020-04-22 18:32:58 +0200

    Initial Settings.js

---
+++ src/components/Settings/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Settings.css';
+
+export class Settings extends React.Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
